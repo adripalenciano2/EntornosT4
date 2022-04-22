38554fbff36e0d02c7eba24f93495b73313e5aa6
adripalenciano2 <alu1733@ieselcaminas.org>
2022-04-22 21:07:28 +0200

    Commit Testing: Documentacion (JavaDoc)

--- src/com/company/Primos.java
+++ src/com/company/Primos.java
@@ -1,6 +1,21 @@
 package com.company;
+
+/**
+ * Clase de números Primos:
+ * Esta clase contiene m&eacute;todos para generar los n&uacute;meros primos existentes
+ * entre 1 y un n&uacute;mero dado.
+ * @author Adri&aacute;n Palenciano Garc&iacute;a
+ * @version Branch3-documentacion
+ */
+
 public class Primos {
     // Generar números primos de 1 a max
+    /**
+     * Generar primos:
+    *Método para generar los números primos desde 1 hasta el número indicado.
+     *@param max El parámetro max define el número hasta el que vamos a generar primos.
+     * @return Los números primos de 1 hasta max
+    */
     public static int[] generarPrimos (int max) {
         if (max >= 2) {
             // Declaraciones
@@ -26,7 +41,16 @@ public class Primos {
         }
     }
 
-    static int[] vectorPrimos(int dim, boolean[] esPrimo, int cuenta) {
+    /**
+     * Vector de números primos:
+     * Método para generar un vector que contenga los números primos indicados a partir de un vector booleano.
+     * @param dim El tamaño del vector booleano.
+     * @param esPrimo El vector booleano (La posición indica el número y el contenido si es primo o no).
+     * @param cuenta La cantidad de números primos que hay en el vector booleano.
+     * @return Vector de enteros que contiene los números primos generados.
+     */
+
+    public static int[] vectorPrimos(int dim, boolean[] esPrimo, int cuenta) {
         int j;
         int i;
         int[] primos = new int[cuenta];
@@ -38,7 +62,14 @@ public class Primos {
         return primos;
     }
 
-    static int cuentaPrimos(int dim, boolean[] esPrimo) {
+    /**
+     * Recuento de números primos:
+     * Método que calcula la cantidad de números primos que indica un vector booleano.
+     * @param  dim El tamaño del vector booleano.
+     * @param esPrimo El vector booleano (La posición indica el número y el contenido si es primo o no).
+     * @return La cantidad de números primos.
+     */
+    public static int cuentaPrimos(int dim, boolean[] esPrimo) {
         int i;
         int cuenta = 0;
         for (i=0; i< dim; i++) {
@@ -49,14 +80,26 @@ public class Primos {
         return cuenta;
     }
 
-    static void inicializaArray(int dim, boolean[] esPrimo) {
+    /**
+     * Inicialización del vector booleano:
+     * Método para inicializar un vector booleano, poniendo sus valores a true.
+     * @param  dim El tamaño del vector booleano.
+     * @param esPrimo El vector booleano (La posición indica el número y el contenido si es primo o no).
+     */
+    public static void inicializaArray(int dim, boolean[] esPrimo) {
         int i;
         for (i=0; i< dim; i++) {
             esPrimo[i] = true;
         }
     }
 
-    static void criba(int dim, boolean[] esPrimo) {
+    /**
+     * Criba de números primos:
+     * Método que da valor al vector booleano, en función de si la posición (número) es primo o no.
+     * @param  dim El tamaño del vector booleano.
+     * @param esPrimo El vector booleano (La posición indica el número y el contenido si es primo o no).
+     */
+    public static void criba(int dim, boolean[] esPrimo) {
         esPrimo[0] = esPrimo[1] = false;
         int j;
         int i;
